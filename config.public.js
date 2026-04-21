5857f50f3eb31a462c99abf1b4235f6e55125120
AndreasAdam <andreas.adam@student.howest.be>
2026-04-21 13:54:16 +0200

    Add public configuration file for Supabase integration and update script reference

---
+++ config.public.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: 'https://sjkmcjmdsurkbbibblwq.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqa21jam1kc3Vya2JiaWJibHdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3NjM1MDAsImV4cCI6MjA5MjMzOTUwMH0.b_o9GWIeuxTN_bgJnqKb54es28MiZUczRko5Q4Fd_vQ',
+};
